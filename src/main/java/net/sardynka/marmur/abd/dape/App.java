d0e82fe5b2f40fa7dff80ef3a88f8dbcf37d4e6e
Marmur <jackandy@op.pl>
2011-01-22 04:16:51 +0100

    first commit

---
+++ src/main/java/net/sardynka/marmur/abd/dape/App.java
@@ -0,0 +1,13 @@
+package net.sardynka.marmur.abd.dape;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
