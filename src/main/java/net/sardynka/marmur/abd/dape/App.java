143da209d64e92903e906bd7abc885837320d3c2
Dajakk <pilithewizard@gmail.com>
2011-01-24 10:08:59 +0100

    rest

--- src/main/java/net/sardynka/marmur/abd/dape/App.java
+++ src/main/java/net/sardynka/marmur/abd/dape/App.java
@@ -1,13 +1,36 @@
 package net.sardynka.marmur.abd.dape;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
+import org.apache.cxf.jaxrs.provider.JSONProvider;
+
+public class App {
+	public static void main(String[] args) throws FileNotFoundException,
+			IOException {
+		String address = null;
+		if (args.length == 0) {
+			address = "http://127.0.0.1:63563/";
+		} else if (args.length == 1) {
+			address = args[0];
+		} else {
+			System.out.println("Wrong number of parameters");
+			System.out.println();
+			System.out.println("Usage: java App.java [service address]");
+			System.out
+					.println("  service address - default: http://127.0.0.1:63563");
+			System.exit(13);
+		}
+		StorageServiceRest storageServiceRest = new StorageServiceRest();
+
+		JAXRSServerFactoryBean restServer = new JAXRSServerFactoryBean();
+		restServer.setResourceClasses(String.class);
+		restServer.setServiceBeans(storageServiceRest);
+		restServer.setAddress(address);
+		JSONProvider provider = new JSONProvider();
+		provider.setDropRootElement(true);
+		restServer.setProvider(provider);
+		restServer.create();
+	}
 }
--- src/main/java/net/sardynka/marmur/abd/dape/StorageServiceRest.java
+++ src/main/java/net/sardynka/marmur/abd/dape/StorageServiceRest.java
@@ -9,36 +9,41 @@ import java.util.Properties;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 import net.sardynka.marmur.abd.dape.estimator.implementation.EstimatorBuilder;
 import net.sardynka.marmur.abd.dape.estimator.interfaces.IEstimator;
 
 @Path("/e")
-@Produces("application/json")
+// @Produces("application/json")
 public class StorageServiceRest {
 	IEstimator ie;
-	
+
 	public StorageServiceRest() throws FileNotFoundException, IOException {
 		Properties props = new Properties();
 		props.load(new FileInputStream(new File("res/configuration.properties")));
 		ie = new EstimatorBuilder().setProperties(props).build();
 	}
-	
+
 	@GET
 	@Path("/write/host/{host}/size/{fileSize}")
-	public Long getWriteEstimation(@PathParam("host") String host, @PathParam("fileSize") String filesize) {
-		System.out.println(host);
-		System.out.println(filesize);
-		System.out.println("alamakota");
-		//return new Long(1);
-		//return Long.parseLong(filesize);
-		return new Long(ie.getWriteEstimation(host, Long.parseLong(filesize)));
+	public Long getWriteEstimation(@PathParam("host") String host,
+			@PathParam("fileSize") long filesize) {
+		return new Long(ie.getWriteEstimation(host, filesize));
 	}
 
 	@GET
 	@Path("/read/host/{host}/size/{fileSize}")
-	public Long getReadEstimation(@PathParam("host") String host, @PathParam("fileSize") String filesize) {
-		return new Long(ie.getReadEstimation(host, Long.parseLong(filesize)));
+	public Long getReadEstimation(@PathParam("host") String host,
+			@PathParam("fileSize") long filesize) {
+		return new Long(ie.getReadEstimation(host, filesize));
+	}
+
+	@GET
+	@Path("/push/host/{host}/written/{written}/read/{read}")
+	public Response pushUpdateData(@PathParam("host") String host,
+			@PathParam("written") long KBwritten, @PathParam("read") long KBred) {
+		ie.getDataCollector().pushUpdateData(host, KBwritten, KBred);
+		return Response.ok(host).build();
 	}
 }
